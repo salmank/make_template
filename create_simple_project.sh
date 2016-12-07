5e755c399093e8a8b46cdcbc3399e97c2fea8372
Salman Khan <salmankhan1@Salmans-MBP.home>
2016-12-07 14:25:31 +0000

    Initial commit

---
+++ create_simple_project.sh
@@ -0,0 +1,16 @@
+PATH=$1
+PROJNAME=`/usr/bin/basename $1`
+SOURCE_PATH=`/usr/bin/dirname ${BASH_SOURCE[0]}`
+
+echo "Creating project ${PROJNAME} at ${PATH}"
+
+if [ -d "$PATH" ]; then
+    echo "Directory already exists"
+    exit
+fi
+
+/bin/mkdir ${PATH} ${PATH}/src ${PATH}/test
+
+/usr/bin/sed "s/__BINNAME__/${PROJNAME}/g" ${SOURCE_PATH}/Makefile.template > ${PATH}/Makefile
+/usr/bin/sed "s/__BINNAME__/${PROJNAME}/g" ${SOURCE_PATH}/src/template.cc >  ${PATH}/src/${PROJNAME}.cc
+/usr/bin/sed "s/__BINNAME__/${PROJNAME}/g" ${SOURCE_PATH}/test/template_test.cc > ${PATH}/test/${PROJNAME}_test.cc
