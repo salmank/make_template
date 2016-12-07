e3b09d8e266de5927b8729a9964c8775f109640e
Salman Khan <salman.khan@gmail.com>
2016-12-07 15:00:24 +0000

    create git

--- create_simple_project.sh
+++ create_simple_project.sh
@@ -14,3 +14,8 @@ fi
 /usr/bin/sed "s/__BINNAME__/${PROJNAME}/g" ${SOURCE_PATH}/Makefile.template > ${PATH}/Makefile
 /usr/bin/sed "s/__BINNAME__/${PROJNAME}/g" ${SOURCE_PATH}/src/template.cc >  ${PATH}/src/${PROJNAME}.cc
 /usr/bin/sed "s/__BINNAME__/${PROJNAME}/g" ${SOURCE_PATH}/test/template_test.cc > ${PATH}/test/${PROJNAME}_test.cc
+/bin/cp ${SOURCE_PATH}/gitignore_template ${PATH}/.gitignore
+cd ${PATH}
+/usr/bin/git init
+/usr/bin/git add Makefile src/*.cc test/*.cc .gitignore
+/usr/bin/git commit -m "Initial commit for ${PROJNAME}"
